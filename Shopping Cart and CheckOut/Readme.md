8d1c20cf4da2b8c248aebe0f750029fe3fe8bb3e
Julliane Tampus <julliane.tampus123@gmail.com>
2024-09-23 00:20:26 +0800

    adding controllers,Database,router

---
+++ Shopping Cart and CheckOut/Readme.md
@@ -0,0 +1,4 @@
+composer instsallation packages
+
+- composer require firebase/php-jwt
+- composer require vlucas/phpdotenv
--- Shopping Cart and CheckOut/index.php
+++ Shopping Cart and CheckOut/index.php
@@ -1,12 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Hello World</h1>
-    <p>Cyrel test</p>
-</body>
-</html>
+<?php
+
+$user = array(
+    "user" => "julliane",
+    "age" => 21
+);
+
+
+echo json_encode($user);
+
+
